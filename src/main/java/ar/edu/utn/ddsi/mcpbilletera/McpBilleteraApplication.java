ae52bd3ff0aebf1e1d809b496dfbf6cf9a6623d4
santinocarrizoo04 <sacarrizo@frba.utn.edu.ar>
2026-07-02 16:20:41 -0300

    initial commit

---
+++ src/main/java/ar/edu/utn/ddsi/mcpbilletera/McpBilleteraApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.ddsi.mcpbilletera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpBilleteraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpBilleteraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/edu/utn/ddsi/mcpbilletera/McpBilleteraApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.ddsi.mcpbilletera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpBilleteraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
